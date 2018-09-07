1944e925c70c8fd7f891510eb18e2589ffecea48
Rahul Bhandari <bhandar3@myumanitoba.ca>
2018-09-07 16:02:18 -0400

    Add files via upload

--- 2016/Q4-2016.py
+++ 2016/Q4-2016.py
@@ -1,23 +1,28 @@
-from datetime import datetime,date
 import timeit
 start = timeit.default_timer()
 MORNING_RUSH_HOUR_BEGIN = 420
 MORNING_RUSH_HOUR_END = 600
 EVENING_RUSH_HOUR_BEGIN = 900
-EVENING_RUSH_HOUR_ENDS = 1140
+EVENING_RUSH_HOUR_END = 1140
 
 
 def check_rush_hour(departure, arrival):
     extra_time = 0
     if MORNING_RUSH_HOUR_BEGIN <= arrival <= MORNING_RUSH_HOUR_END:
         if MORNING_RUSH_HOUR_BEGIN <= departure <= MORNING_RUSH_HOUR_END:
+            extra_time = int((240 - (MORNING_RUSH_HOUR_END - departure_time)) / 2)
+        else:
             extra_time = min(arrival-MORNING_RUSH_HOUR_BEGIN, arrival-departure)
     elif MORNING_RUSH_HOUR_BEGIN <= departure <= MORNING_RUSH_HOUR_END:
         extra_time = MORNING_RUSH_HOUR_END-departure
-    elif EVENING_RUSH_HOUR_BEGIN <= arrival <= EVENING_RUSH_HOUR_ENDS:
+    elif EVENING_RUSH_HOUR_BEGIN <= arrival <= EVENING_RUSH_HOUR_END:
+        if EVENING_RUSH_HOUR_BEGIN <= departure <= EVENING_RUSH_HOUR_END:
+            extra_time = int((240 - (EVENING_RUSH_HOUR_END - departure_time)) / 2)
+        else:
+            extra_time = min(arrival-EVENING_RUSH_HOUR_BEGIN, arrival-departure)
         extra_time = min(arrival-EVENING_RUSH_HOUR_BEGIN, arrival-departure)
-    elif EVENING_RUSH_HOUR_BEGIN <= departure_time <= EVENING_RUSH_HOUR_ENDS:
-        extra_time = EVENING_RUSH_HOUR_ENDS - departure
+    elif EVENING_RUSH_HOUR_BEGIN <= departure_time <= EVENING_RUSH_HOUR_END:
+        extra_time = EVENING_RUSH_HOUR_END - departure
     return extra_time
 
 
