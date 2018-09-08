91461540503fae3f8220e5b07a436a970b4a4628
Rahul Bhandari <bhandar3@myumanitoba.ca>
2018-09-08 17:31:28 -0400

    Comments explaining the rush hour cases.

--- 2016/Q4-2016.py
+++ 2016/Q4-2016.py
@@ -8,13 +8,19 @@ EVENING_RUSH_HOUR_END = 1140
 
 def check_rush_hour(departure, arrival):
     extra_time = 0
+    # Case 1: Arrival time between morning rush hour
     if MORNING_RUSH_HOUR_BEGIN <= arrival <= MORNING_RUSH_HOUR_END:
+        # Case 2: Departure time between morning rush hour along with arrival time
         if MORNING_RUSH_HOUR_BEGIN <= departure <= MORNING_RUSH_HOUR_END:
+            #extra time has a max value of 120. An alogrithm below is designed to calculate that
             extra_time = int((120 - (MORNING_RUSH_HOUR_END - arrival_time)) / 2)+(MORNING_RUSH_HOUR_END - arrival_time)
+        # Case 3: Departure time not between the morning rush hour
         else:
             extra_time = min(arrival-MORNING_RUSH_HOUR_BEGIN, arrival-departure)
+    # Case 4: Departure time between rush hour but not the arrival time
     elif MORNING_RUSH_HOUR_BEGIN <= departure <= MORNING_RUSH_HOUR_END:
         extra_time = MORNING_RUSH_HOUR_END-departure
+    # The next cases are similar to the previous ones, just for the evening rush hours
     elif EVENING_RUSH_HOUR_BEGIN <= arrival <= EVENING_RUSH_HOUR_END:
         if EVENING_RUSH_HOUR_BEGIN <= departure <= EVENING_RUSH_HOUR_END:
             extra_time = int((120 - (EVENING_RUSH_HOUR_END - arrival_time)) / 2)+(EVENING_RUSH_HOUR_END - arrival_time)
